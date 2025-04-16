d220ee0c51c52ba3361a0bc62e14805e9d02cdf9
BasHuw <huuphan0509@gmail.com>
2025-04-16 16:10:23 +0700

    init

---
+++ src/java/vn/edu/fpt/dao/UserDao.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.edu.fpt.dao;
+
+/**
+ *
+ * @author MTTTT
+ */
+public class UserDao {
+    public void saveUser(){
+    }
+    
+     public void editUser(){
+    }
+     
+      public void getUser(int id){
+    }
+      
+       public void listUser(){
+    }
+       
+        public void deleteUser(){
+    }
+}
