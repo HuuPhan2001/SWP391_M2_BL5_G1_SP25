4cdde3a6354d780f41682e4323cad55148f0a422
Nguyen Anh Dung (K15 HL) <dungnahe150969@fpt.edu.vn>
2025-04-21 03:27:17 +0700

    Test DbContext

---
+++ src/java/vn/edu/fpt/dao/DbContext.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.edu.fpt.dao;
+
+
+/**
+ *
+ * @author MTTTT
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author admin
+ */
+public class DbContext {
+    protected Connection connection;
+    public DbContext()
+    {
+        try {
+            String user = "sa";
+            String pass = "123";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=OnLineShop;encrypt=true;trustServerCertificate=true";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DbContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
