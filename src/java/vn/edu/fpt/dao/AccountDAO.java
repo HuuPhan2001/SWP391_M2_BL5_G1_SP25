34b491db221b9011febfc21c0d1633ce328ab462
Solairethedk <dungnahe150969@fpt.edu.vn>
2025-04-21 11:24:23 +0700

    add account

--- src/java/vn/edu/fpt/dao/AccountDAO.java
+++ src/java/vn/edu/fpt/dao/AccountDAO.java
@@ -21,7 +21,7 @@ public class AccountDAO extends DbContext {
                    "FROM [user] u " +
                    "WHERE u.user_name = ? AND u.user_password = ?";
     try {
-            PreparedStatement st = connection.prepareStatement(sql);
+                PreparedStatement st = connection.prepareStatement(sql);
             st.setString(1, username);
             st.setString(2, password);
             
@@ -74,17 +74,51 @@ public class AccountDAO extends DbContext {
 //    return null;
 }
 
-    public void addAccount(String username, String password) {
-        String out = "Dang ky thanh cong";
-        String sql = "INSERT INTO dbo.account VALUES (N'" + username + "', N'" + password + "', 'user')";
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
-            st.executeQuery();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
+    public boolean addAccount(User user) {
+    String sql = "INSERT INTO [user] (user_name, user_email, user_password, phone, status, role_id, create_at, update_at) "
+               + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        ps.setString(1, user.getUserName());
+        ps.setString(2, user.getUserEmail());
+        ps.setString(3, user.getUserPassword());
+        ps.setString(4, user.getPhone());
+        ps.setInt(5, user.getStatus());
+        ps.setObject(6, user.getRoleId());
+        ps.setTimestamp(7, user.getCreateAt());
+        ps.setTimestamp(8, user.getUpdateAt());
 
+        return ps.executeUpdate() > 0;
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+    
+    public boolean isEmailExists(String email) {
+    String sql = "SELECT user_id FROM [user] WHERE user_email = ?";
+    try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        ps.setString(1, email);
+        ResultSet rs = ps.executeQuery();
+        return rs.next();
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+    
+    public boolean isUsernameExists(String username) {
+    String sql = "SELECT user_id FROM [user] WHERE user_name = ?";
+    try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        ps.setString(1, username);
+        ResultSet rs = ps.executeQuery();
+        return rs.next();
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+    
     public void updateAccount(String username, String newpass) {
         String sql = "UPDATE dbo.account SET password = ? WHERE username = ? ";
         try {
